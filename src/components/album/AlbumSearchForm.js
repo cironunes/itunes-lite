271f213078741ee87f9d6c20fc0e7fdcbc1f79f6
Fernando Souza <nandosouzafilho@gmail.com>
2018-05-20 19:44:21 +0200

    fix(AlbumSearchForm): prevents form submission. fixes #5

--- src/components/album/AlbumSearchForm.js
+++ src/components/album/AlbumSearchForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export const AlbumSearchForm = props => (
-  <form className="App-form">
+  <form className="App-form" onSubmit={e => e.preventDefault()}>
     <h2 className="App-subtitle">Have fun!</h2>
     <p className="App-form__hint">Find your favorite albuns and get to know more about them</p>
     <div className="SearchForm">
